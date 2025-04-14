f1ef2c2ea0d895870799eece10b0ca556a186d22
Swarnendu <swarnendumaity633@gmail.com>
2025-04-14 14:23:13 +0530

    Add ts-node-dev as a dev dependency, update app configuration to include BASE_PATH, and implement a basic Hello World endpoint in the Express application.

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -3,6 +3,7 @@ import { getEnv } from "../utils/getEnv";
 const appConfig = () => ({
     PORT: getEnv("PORT", "8000"),
     NODE_ENV: getEnv("NODE_ENV", "development"),
+    BASE_PATH: getEnv("BASE_PATH", "/api"),
     DATABASE_URL: getEnv("DATABASE_URL"),
     JWT_SECRET: getEnv("JWT_SECRET"),
     JWT_EXPIRES_IN: getEnv("JWT_EXPIRES_IN", "1d"),
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,15 @@ import config from "./config/appConfig";
  
 
 const app = express();
+const BASE_PATH = config.BASE_PATH;
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
 
 app.listen(config.PORT, async () => {
      

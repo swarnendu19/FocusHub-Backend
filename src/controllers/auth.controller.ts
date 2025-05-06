f4073084808991c8e9b962b3ed3c233f064670a3
Swarnendu <swarnendumaity633@gmail.com>
2025-05-06 20:23:30 +0530

    chore: update dependencies and add Docker configurations for development, testing, and production environments

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,8 +5,8 @@ import exclude from '../utils/exclude';
 import { User } from '@prisma/client';
 
 const register = catchAsync(async (req, res) => {
-  const { email, password } = req.body;
-  const user = await userService.createUser(email, password);
+  const { email, password, name } = req.body;
+  const user = await userService.createUser(email, password, name);
   const userWithoutPassword = exclude(user, ['password', 'createdAt', 'updatedAt']);
   const tokens = await tokenService.generateAuthTokens(user);
   res.status(httpStatus.CREATED).send({ user: userWithoutPassword, tokens });

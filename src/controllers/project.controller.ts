4cfbc7e8bcb6297d3ea8c885b0605e367a606194
Swarnendu <swarnendumaity633@gmail.com>
2025-05-10 23:04:06 +0530

    refactor: improve code formatting and consistency in project controller

--- src/controllers/project.controller.ts
+++ src/controllers/project.controller.ts
@@ -1,20 +1,19 @@
-import { projectService } from "../services";
-import catchAsync from "../utils/catchAsync"
-import { Request, Response } from "express"
-import httpStatus from "http-status";
+import { projectService } from '../services';
+import catchAsync from '../utils/catchAsync';
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
 
-const createProject = catchAsync(async(req: Request, res: Response)=>{
-    const project = await projectService.createProject(req.body);
-    res.status(httpStatus.CREATED).json(project);
-})
+const createProject = catchAsync(async (req: Request, res: Response) => {
+  const project = await projectService.createProject(req.body);
+  res.status(httpStatus.CREATED).json(project);
+});
 
-
-const getProjectsById = catchAsync(async(req: Request, res: Response)=>{
-    const project = await projectService.getProjectsById(req.params.projectId);
-    res.status(httpStatus.OK).json(project);
-})
+const getProjectsById = catchAsync(async (req: Request, res: Response) => {
+  const project = await projectService.getProjectsById(parseInt(req.params.projectId));
+  res.status(httpStatus.OK).json(project);
+});
 
 export default {
-    createProject,
-    getProjectsById,
-}
+  createProject,
+  getProjectsById,
+};

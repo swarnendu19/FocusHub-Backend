d244b7b4a016e71785244f642b271b458ec4d8d7
Swarnendu <swarnendumaity633@gmail.com>
2025-05-20 00:32:21 +0530

    feat: Add MongoDB connection logic in db/index.ts

---
+++ db/index.ts
@@ -0,0 +1,54 @@
+import { MongoClient, Db } from "mongodb";
+
+let cacheDb: Db | null = null;
+let client: MongoClient | null = null;
+
+
+async function connectToDatabase() {
+    if(cacheDb){
+        try {
+            await cacheDb.command({ ping: 1 });
+            return cacheDb;
+        } catch (error) {
+            console.log("Cache DB connection failed, reconnecting...");
+            cacheDb = null;
+            if(client) {
+                await client.close();
+            }
+        }
+
+        try {
+            client = await MongoClient.connect(process.env.MONGO_URI, {
+                connectTimeoutMS: 10000,
+                socketTimeoutMS: 10000,
+                serverSelectionTimeoutMS: 10000,
+                maxPoolSize: 10,
+                minPoolSize: 5,
+                rentryWrites: true,
+                rentryReads: true,
+            });
+
+            client.on('connected', ()=>{
+                console.log("MongoDB connected Successfully");
+            })
+
+            client.on('error', (error)=>{
+                console.log("MongoDB connection error: ", error);
+                cacheDb = null;
+            });
+
+            const db = client.db('usersDB');
+            cacheDb = db;
+            return db;
+          
+        } catch (error) {
+            console.error("MongoDB connection error: ", error);
+            cacheDb = null;
+            if(client) {
+                await client.close();
+            }
+            throw error;
+        }
+    }
+
+}

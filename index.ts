7d85d11579e78d867ed56c09bb631db119817074
Swarnendu <swarnendumaity633@gmail.com>
2025-05-20 12:06:25 +0530

    feat: Add Express setup with middleware for JSON parsing in index.ts and update package.json with new dependencies

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log("Hello via Bun!");
+import express from 'express';
+import dotenv from 'dotenv';
+import session from 'express-session';
+import cors from 'cors';
+
+dotenv.config();
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+    cors({
+      origin: process.env.CLIENT || 'http://localhost:3000',
+      credentials: true, // Allow credentials
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
+      allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+      exposedHeaders: ['Content-Type', 'Authorization'] // Exposed headers
+    })
+  );
+

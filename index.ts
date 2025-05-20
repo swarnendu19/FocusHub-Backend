3f46dea46badaed75c33af315baf4bcd96714843
Swarnendu <swarnendumaity633@gmail.com>
2025-05-20 12:18:59 +0530

    Add MongoDB connection setup and server start logic in index.ts

--- index.ts
+++ index.ts
@@ -2,13 +2,19 @@ import express from 'express';
 import dotenv from 'dotenv';
 import session from 'express-session';
 import cors from 'cors';
-
 dotenv.config();
+
+import { connectToDatabase } from './db';
 
 
 const app = express();
 
 app.use(express.json());
+const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017';
+if(!mongoUri){
+    console.error("Mongo URI is not defined");
+    process.exit(1);
+}
 
 app.use(
     cors({
@@ -19,4 +25,20 @@ app.use(
       exposedHeaders: ['Content-Type', 'Authorization'] // Exposed headers
     })
   );
+
+const PORT = process.env.PORT || 5000;
+const startServer = async () => {
+    try{
+        await connectToDatabase();
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    }
+    catch (error) {
+        console.error("Error starting server: ", error);
+        process.exit(1);
+    }
+}
+
+startServer();
 

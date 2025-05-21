74a72b06d9897b3246cef1babdb631ac514b6198
Swarnendu <swarnendumaity633@gmail.com>
2025-05-21 09:20:51 +0530

    Refactor MongoDB connection setup in db/index.ts

--- db/index.ts
+++ db/index.ts
@@ -5,6 +5,8 @@ let client: MongoClient | null = null;
 
 
 async function connectToDatabase() {
+    // console.log("Reached connectToDatabase");
+    
     if(cacheDb){
         try {
             await cacheDb.command({ ping: 1 });
@@ -16,41 +18,40 @@ async function connectToDatabase() {
                 await client.close();
             }
         }
-
-        try {
-            client = await MongoClient.connect(process.env.MONGO_URI, {
-                connectTimeoutMS: 10000,
-                socketTimeoutMS: 10000,
-                serverSelectionTimeoutMS: 10000,
-                maxPoolSize: 10,
-                minPoolSize: 5,
-                retryWrites: true,
-                retryReads: true,
-            });
-
-            client.on('connected', ()=>{
-                console.log("MongoDB connected Successfully");
-            })
-
-            client.on('error', (error)=>{
-                console.log("MongoDB connection error: ", error);
-                cacheDb = null;
-            });
-
-            const db = client.db('usersDB');
-            cacheDb = db;
-            return db;
-          
-        } catch (error) {
-            console.error("MongoDB connection error: ", error);
-            cacheDb = null;
-            if(client) {
-                await client.close();
-            }
-            throw error;
-        }
+        // console.log("Here");
     }
+    
+    try {
+        // console.log("Inside try");
+        
+        client = await MongoClient.connect(process.env.MONGODB_URI, {
+            connectTimeoutMS: 10000,
+            socketTimeoutMS: 10000,
+            serverSelectionTimeoutMS: 10000,
+            maxPoolSize: 10,
+            minPoolSize: 5,
+            retryWrites: true,
+            retryReads: true,
+        });
+       
+        console.log("MongoDB connected Successfully");
+        client.on('error', (error)=>{
+            console.log("MongoDB connection error: ", error);
+            cacheDb = null;
+        });
 
+        const db = client.db('usersDB');
+        cacheDb = db;
+        return db;
+      
+    } catch (error) {
+        console.error("MongoDB connection error: ", error);
+        cacheDb = null;
+        if(client) {
+            await client.close();
+        }
+        throw error;
+    }
 }
 
 
